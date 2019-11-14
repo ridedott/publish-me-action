f359c8b99d2c741307499f0ec589ec7bebf826eb
Ed <oleg@ridedott.com>
2019-11-14 23:19:24 +0100

    chore: update dist folder

--- dist/config/plugins.d.ts
+++
@@ -1,3 +0,0 @@
-import { PluginSpec } from 'semantic-release';
-export declare const plugins: PluginSpec[];
-//# sourceMappingURL=plugins.d.ts.map
--- dist/tasks.d.ts
+++
@@ -1,7 +0,0 @@
-import { Result } from 'semantic-release';
-export declare enum Commands {
-    CreateGitIgnoreBackup = "createGitIgnoreBackup",
-    RemoveNpmrc = "removeNpmrc"
-}
-export declare const reportResults: (result: Result) => Promise<void>;
-export declare const runTask: (task: Commands) => Promise<void>;
