092f07f80a929d7367c457b7674093c545b135c1
Oleg Koval <orthodoz@gmail.com>
2019-11-18 13:57:52 +0100

    chore: update dist for action master version

--- dist/config/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { parserOptions } from './parserOptions';
-import { plugins } from './plugins';
-import { releaseRules } from './releaseRules';
-import { transform } from './transform';
-export { parserOptions, plugins, releaseRules, transform };
-//# sourceMappingURL=index.d.ts.map
