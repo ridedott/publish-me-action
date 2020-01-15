be9d9144e3be907016f345819a49c2afc896a939
Ed <oleg@ridedott.com>
2020-01-15 11:36:33 +0100

    chore: build the latest version

--- dist/config/plugins.d.ts
+++
@@ -1,5 +0,0 @@
-import { PluginSpec } from 'semantic-release';
-export declare const generatePlugins: (options: {
-    scriptPath: string | undefined;
-}) => PluginSpec[];
-//# sourceMappingURL=plugins.d.ts.map
