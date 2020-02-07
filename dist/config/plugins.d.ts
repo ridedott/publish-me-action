316dfb977dbcbb578b9ca211007564f54d007d20
Alex Cazacu <alexc@ridedott.com>
2020-02-07 11:19:02 +0100

    chore: refactor project to enable push to multiple registries

--- dist/config/plugins.d.ts
+++
@@ -1,6 +0,0 @@
-import { PluginSpec } from 'semantic-release';
-export declare const generatePlugins: (options: {
-    publishToNpm: boolean;
-    scriptPath: string | undefined;
-}) => PluginSpec[];
-//# sourceMappingURL=plugins.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,76 @@
-import { setFailed } from '@actions/core';
-import { env as environment } from 'process';
-import * as semanticRelease from 'semantic-release';
+// import { setFailed } from '@actions/core';
+// import { env as environment } from 'process';
+// import * as semanticRelease from 'semantic-release';
+//
+// import {
+//   generatePlugins,
+//   parserOptions,
+//   releaseRules,
+//   transform,
+// } from './config';
+// import {
+//   handleBranchFlag,
+//   handleDebugFlag,
+//   handleDryRunFlag,
+//   handleScriptPathFlag,
+// } from './optionsHandlers';
+// import { Commands, reportResults, runTask } from './tasks';
+//
+// if (handleDebugFlag() === true) {
+//   /* eslint-disable @typescript-eslint/no-require-imports */
+//   /* eslint-disable-next-line global-require */
+//   require('debug').enable('semantic-release:*');
+//   /* eslint-enable @typescript-eslint/no-require-imports */
+// }
+//
+// const main = async (): Promise<void> => {
+//   await Promise.all([
+//     runTask(Commands.PreInstallPlugins),
+//     runTask(Commands.RemoveNpmrc),
+//   ]);
+//
+//   const result = await semanticRelease({
+//     /* eslint-disable unicorn/prevent-abbreviations */
+//     ci: false,
+//     ...handleBranchFlag(),
+//     ...handleDryRunFlag(),
+//     parserOpts: parserOptions,
+//     plugins: generatePlugins({
+//       publishToNpm: environment.NPM_TOKEN !== undefined,
+//       scriptPath: await handleScriptPathFlag(),
+//     }),
+//     releaseRules,
+//     writerOpts: { transform },
+//     /* eslint-enable unicorn/prevent-abbreviations */
+//   });
+//
+//   await reportResults(result);
+// };
+//
+// main().catch((error: Error): void => {
+//   setFailed(`An unexpected error occurred: ${error}, ${error.stack}.`);
+// });
 
-import {
-  generatePlugins,
-  parserOptions,
-  releaseRules,
-  transform,
-} from './config';
-import {
-  handleBranchFlag,
-  handleDebugFlag,
-  handleDryRunFlag,
-  handleScriptPathFlag,
-} from './optionsHandlers';
-import { Commands, reportResults, runTask } from './tasks';
+import { getInput, setFailed } from '@actions/core';
 
-if (handleDebugFlag() === true) {
-  /* eslint-disable @typescript-eslint/no-require-imports */
-  /* eslint-disable-next-line global-require */
-  require('debug').enable('semantic-release:*');
-  /* eslint-enable @typescript-eslint/no-require-imports */
+export enum Configuration {
+  branch = 'BRANCH',
+  dryRun = 'DRY_RUN',
+  debug = 'DEBUG',
 }
 
 const main = async (): Promise<void> => {
-  await Promise.all([
-    runTask(Commands.PreInstallPlugins),
-    runTask(Commands.RemoveNpmrc),
-  ]);
+  const branch: string = getInput(Configuration.branch);
+  const isDryRun: boolean = getInput(Configuration.dryRun) === 'true';
+  const isDebug: boolean = getInput(Configuration.debug) === 'true';
 
-  const result = await semanticRelease({
-    /* eslint-disable unicorn/prevent-abbreviations */
-    ci: false,
-    ...handleBranchFlag(),
-    ...handleDryRunFlag(),
-    parserOpts: parserOptions,
-    plugins: generatePlugins({
-      publishToNpm: environment.NPM_TOKEN !== undefined,
-      scriptPath: await handleScriptPathFlag(),
-    }),
-    releaseRules,
-    writerOpts: { transform },
-    /* eslint-enable unicorn/prevent-abbreviations */
-  });
-
-  await reportResults(result);
+  // eslint-disable-next-line no-console
+  console.log(JSON.stringify({
+    branch, isDebug, isDryRun
+  }));
 };
 
-main().catch((error: Error): void => {
-  setFailed(`An unexpected error occurred: ${error}, ${error.stack}.`);
-});
+main()
+  .catch((error: Error): void => {
+    setFailed(JSON.stringify(error));
+  });
