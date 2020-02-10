e11933b54da97d16df461b42d3ee9bb55e680151
Alex Cazacu <alexc@ridedott.com>
2020-02-10 12:06:28 +0100

    chore: refactor project to enable push to multiple registries

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,42 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
+const process_1 = require("process");
+const semanticRelease = require("semantic-release");
+const commitAnalyzerParserOptions = {
+    mergeCorrespondence: ['id', 'source'],
+    /* eslint-disable prefer-named-capture-group */
+    /* eslint-disable require-unicode-regexp */
+    mergePattern: /^Merge pull request #(\d+) from (.*)$/,
+};
+const commitAnalyzerReleaseRules = [
+    { release: 'patch', type: 'build' },
+    { release: 'patch', type: 'ci' },
+    { release: 'patch', type: 'chore' },
+    { release: 'patch', type: 'docs' },
+    { release: 'patch', type: 'refactor' },
+    { release: 'patch', type: 'style' },
+    { release: 'patch', type: 'test' },
+];
 const main = async () => {
-    console.log('aaa');
+    const cwd = typeof process_1.env.GITHUB_WORKSPACE === 'string'
+        ? process_1.env.GITHUB_WORKSPACE
+        : '/github/workspace';
+    await semanticRelease({
+        ci: true,
+        plugins: [
+            [
+                '@semantic-release/commit-analyzer',
+                {
+                    // eslint-disable-next-line unicorn/prevent-abbreviations
+                    parserOpts: commitAnalyzerParserOptions,
+                    releaseRules: commitAnalyzerReleaseRules,
+                },
+            ],
+        ]
+    }, {
+        cwd
+    });
 };
 main().catch((error) => {
     core_1.setFailed(JSON.stringify(error));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
 import { setFailed } from '@actions/core';
+import { env as environment } from 'process';
+import * as semanticRelease from 'semantic-release';
+
+const commitAnalyzerParserOptions = {
+  mergeCorrespondence: ['id', 'source'],
+  /* eslint-disable prefer-named-capture-group */
+  /* eslint-disable require-unicode-regexp */
+  mergePattern: /^Merge pull request #(\d+) from (.*)$/,
+  /* eslint-enable prefer-named-capture-group */
+  /* eslint-enable require-unicode-regexp */
+};
+
+const commitAnalyzerReleaseRules = [
+  { release: 'patch', type: 'build' },
+  { release: 'patch', type: 'ci' },
+  { release: 'patch', type: 'chore' },
+  { release: 'patch', type: 'docs' },
+  { release: 'patch', type: 'refactor' },
+  { release: 'patch', type: 'style' },
+  { release: 'patch', type: 'test' },
+];
 
 const main = async (): Promise<void> => {
-  console.log('aaa');
+  const cwd =
+    typeof environment.GITHUB_WORKSPACE === 'string'
+      ? environment.GITHUB_WORKSPACE
+      : '/github/workspace';
+
+  await semanticRelease(
+    {
+      ci: true,
+      plugins: [
+        [
+          '@semantic-release/commit-analyzer',
+          {
+            // eslint-disable-next-line unicorn/prevent-abbreviations
+            parserOpts: commitAnalyzerParserOptions,
+            releaseRules: commitAnalyzerReleaseRules,
+          },
+        ],
+      ]
+    },
+    {
+      cwd
+    }
+  );
 };
 
 main().catch((error: Error): void => {
