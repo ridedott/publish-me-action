7c7351f7d1ab50e5e180bb38bd73da5b7979ca78
Alex Cazacu <alexc@ridedott.com>
2020-02-10 11:58:18 +0100

    chore: refactor project to enable push to multiple registries

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log('great ssuceces');
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const main = async () => {
+    console.log('aaa');
+};
+main().catch((error) => {
+    core_1.setFailed(JSON.stringify(error));
+});
 //# sourceMappingURL=index.js.map
